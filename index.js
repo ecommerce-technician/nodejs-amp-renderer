00fa31ce05f53a8094c6d55e4018546d5f70d6e1
alex <aw2.alexwood@gmail.com>
2017-04-18 18:20:30 -0700

    adds logs and css

--- index.js
+++ index.js
@@ -8,16 +8,27 @@ fs.readFile( './public/index.html', 'utf8', function (err,data) {
     }
     template.re = /--(.*)--/gi;
     template.variables = data.match(template.re);
+    console.log("\n=== Gathering Assets ===");
     Promise.each(template.variables, function(block){
         var blockName = block.replace(/--/gi,'');
-        console.log(blockName);
+        console.log("--> " + blockName);
         return fs.readFileAsync('./public/' + blockName + '.html', "utf8").then(function (contents) {
             data = data.replace(RegExp(block, "g"), contents);
         });
     }).then(function() {
-        console.log(data);
+        renderPath = 'generated/index.html';
+        console.log("\n=== Rendering Html... ===\n");
+        fs.writeFile(renderPath, data, 'utf8', function(e, data){
+            if(e) {
+                console.log("\n=== Oh dang... I broke. \n try again :( ===\n");
+                console.error(e.stack);
+            } else {
+                console.log("\n=== Done! ===\n Download @ " + __dirname +"/"+ renderPath);
+            }
+        });
     }).catch(function(e) {
-        //console.error(e.stack);
+        console.log("\n=== Darn, I broke. try again :( ===\n");
+        console.error(e.stack);
     });
 
 });
